600dff69cf0bafdfb76dee0945ba9005c4d2b084
BoscoJacinto <boscojac@gmail.com>
2025-04-09 15:59:32 +0530

    Convert status code into a class for modularity, also got signals working
    Signed-off-by: BoscoJacinto <boscojac@gmail.com>

--- main.py
+++ main.py
@@ -11,189 +11,133 @@ from ollama import chat
 from ollama import ChatResponse
 from flwr.client.supernode.app import run_supernode
 
-class SignalClient:
-    def __init__(self, url):
-        self.url = url
-        self.ws = None
-        self.running = False
-        self.thread = None
+status_go = None
+status_cb = None
 
-    def on_message(self, ws, message):
-        try:
-            data = json.loads(message)
-            print(f"Received message: {data}")
-        except json.JSONDecodeError:
-            print(f"Raw message: {message}")
+class StatusClient:
+    def __init__(self, root):
+    	global status_go
+    	self.lib = status_go
+    	self.cb = None
+    	self.root = root
+    	self.uid = ''
+    	self.password = ''
+    	self.device_name = ''
+    	self.display_name = ''
+    	self.wakuv2_nameserver = '8.8.8.8'
+    	self.wakuv2_fleet = 'status.prod'
+    	self.lib.InitializeApplication.argtypes = [ctypes.c_char_p]
+    	self.lib.InitializeApplication.restype = ctypes.c_char_p
+    	self.lib.LoginAccount.argtypes = [ctypes.c_char_p]
+    	self.lib.LoginAccount.restype = ctypes.c_char_p
+    	self.lib.CallRPC.argtypes = [ctypes.c_char_p]
+    	self.lib.CallRPC.restype = ctypes.c_char_p
+    	self.lib.CreateAccountAndLogin.argtypes = [ctypes.c_char_p]
+    	self.lib.CreateAccountAndLogin.restype = ctypes.c_char_p
+    	self.lib.CallPrivateRPC.argtypes = [ctypes.c_char_p]
+    	self.lib.CallPrivateRPC.restype = ctypes.c_char_p
 
-    def on_error(self, ws, error):
-        print(f"Error: {error}")
+    def initApp(self, device_name, cb):
+    	self.device_name = device_name
+    	SIGNAL_CB_TYPE = ctypes.CFUNCTYPE(None, ctypes.c_char_p)
+    	status_go.SetSignalEventCallback.argtypes = [ctypes.c_void_p]
+    	self.cb = SIGNAL_CB_TYPE(cb)
+    	status_go.SetSignalEventCallback(self.cb)
+    	data = {"dataDir": self.root, "mixpanelAppId": "", "mixpanelToken": "", "mediaServerEnableTLS": False, "sentryDSN": "", "logDir": self.root, "logEnabled": True, "logLevel": "INFO", "apiLoggingEnabled": True, "metricsEnabled": True, "metricsAddress": "", "deviceName": self.device_name, "rootDataDir": self.root, "wakuV2LightClient": False, "wakuV2EnableMissingMessageVerification": True, "wakuV2EnableStoreConfirmationForMessagesSent": True}
+    	payload = json.dumps(data).encode('utf-8')
+    	status_go.InitializeApplication(payload)
 
-    def on_close(self, ws, code, msg):
-        print(f"Connection closed. Status: {code}, Message: {msg}")
-        self.running = False
+    def login(self, uid, password):
+    	self.uid = uid
+    	self.password = password
+    	data = {"password": self.password, "keyUid": self.uid, "wakuV2Nameserver": self.wakuv2_nameserver, "wakuV2Fleet": self.wakuv2_fleet}
+    	payload = json.dumps(data).encode('utf-8')
+    	self.lib.LoginAccount(payload)
 
-    def on_open(self, ws):
-        print("Signal connection opened")
-        self.running = True
-        #ws.send(json.dumps({"type": "hello", "message": "Connected!"}))
+    	time.sleep(5)
 
-    def run(self):
-        self.ws = websocket.WebSocketApp(
-            self.url,
-            on_open=self.on_open,
-            on_message=self.on_message,
-            on_error=self.on_error,
-            on_close=self.on_close
-        )
-        
-        self.ws.run_forever()
+    	data = {"method": "wakuext_startMessenger", "params": []}
+    	payload = json.dumps(data).encode('utf-8')
+    	self.lib.CallRPC(payload)
 
-    def start(self):
-        if not self.thread or not self.thread.is_alive():
-            self.thread = threading.Thread(target=self.run)
-            self.thread.daemon = True
-            self.thread.start()
-            print("Signal thread started")
+    def createAccountAndLogin(self, display_name):
+    	self.display_name = display_name
+    	data = {'rootDataDir': self.root, 'kdfIterations': 256, 'deviceName': self.device_name, 'displayName': self.display_name, 'password': self.password, "customizationColor":"blue", 'wakuV2Nameserver':self.wakuv2_nameserver, 'wakuV2Fleet':self.wakuv2_fleet}
+    	payload = json.dumps(data).encode('utf-8')
+    	response = self.lib.CreateAccountAndLogin(payload)
+
+    def sendContactRequest(self, publicKey, message):
+    	data = {"method": "wakuext_sendContactRequest", "params": [{"id": publicKey, "message": message}]}
+    	payload = json.dumps(data).encode('utf-8')
+    	self.lib.CallPrivateRPC(payload)
+
+    def createOneToOneChat(self, chatId):
+    	data = {"method": "chat_createOneToOneChat", "params": ["", chatId, ""]}
+    	payload = json.dumps(data).encode('utf-8')
+    	self.lib.CallPrivateRPC(payload)
+
+    def sendChatMessage(self, chatId, message):
+    	data = {"method": "wakuext_sendChatMessage", "params": [{"chatId": chatId, "text": message, "contentType": 1}]}
+    	payload = json.dumps(data).encode('utf-8')
+    	self.lib.CallPrivateRPC(payload)
+
+    def on_status_cb(self, signal: str):
+    	signal = json.loads(signal)
+    	if signal["type"] == "node.login":
+    		key_uid = signal["event"]["settings"]["key-uid"]
+    		public_key = signal["event"]["settings"]["current-user-status"]["publicKey"]
+    		print(f"Node Login: uid:{key_uid} publicKey:{public_key}")
+    	elif signal["type"] == "message.delivered":
+    		print("Message delivered!")
+    	elif signal["type"] == "messages.new":
+    		print(f"MSGG:{signal["event"]}")
+    		try:
+    			print(f"New Message received!:{signal["event"]["chats"][0]["lastMessage"]["parsedText"][0]["children"][0]["literal"]}")
+    		except KeyError:
+    			pass
+    	return
 
     def stop(self):
-        if self.ws:
-            self.ws.close()
-        self.running = False
-        if self.thread:
-            self.thread.join()
-        print("Signal thread stopped")
+    	print("Exited!")
+    	pass
 
-    def send_message(self, message):
-        if self.ws and self.running:
-            self.ws.send(json.dumps(message))
-            print(f"Sent message: {message}")
+def launchModel():
+	print("\n\n========= Trying to launch model ========\n\n")
+	response: ChatResponse = chat(model='swigg-gemma3:1b')
+	return response['message']['content']
 
-def main():	
+def main():
+	global status_go
+
 	status_go = CDLL("./restaurant_status/libstatus.so.0")
+	status_client = StatusClient(root="./")
 
-	status_go.InitializeApplication.argtypes = [ctypes.c_char_p]
-	status_go.InitializeApplication.restype = ctypes.c_char_p
-	status_go.LoginAccount.argtypes = [ctypes.c_char_p]
-	status_go.LoginAccount.restype = ctypes.c_char_p
-	status_go.CallRPC.argtypes = [ctypes.c_char_p]
-	status_go.CallRPC.restype = ctypes.c_char_p
-	status_go.CreateAccountAndLogin.argtypes = [ctypes.c_char_p]
-	status_go.CreateAccountAndLogin.restype = ctypes.c_char_p
-	status_go.CallPrivateRPC.argtypes = [ctypes.c_char_p]
-	status_go.CallPrivateRPC.restype = ctypes.c_char_p
+	status_client.initApp("restaurant-pc-7", cb=status_client.on_status_cb)
+	time.sleep(0.2)
 
-	signal_client = SignalClient("ws://127.0.0.1:33327/signals")
-	signal_client.start()
-	time.sleep(2)
+	# #status_client.createAccountAndLogin("Restaurant7")
+	# #time.sleep(0.2)
 
-	data = {
-	"dataDir": "./",
-	"mixpanelAppId": "",
-	"mixpanelToken": "",
-	"mediaServerEnableTLS": False,
-	"sentryDSN": "",
-	"logDir": "./",
-	"logEnabled": True,
-	"logLevel": "INFO",
-	"apiLoggingEnabled": True,
-	"metricsEnabled": True,
-	"metricsAddress": "",
-	"deviceName": "restaurant-pc-7",
-	"rootDataDir": "./",
-	"wakuV2LightClient": False,
-	"wakuV2EnableMissingMessageVerification": True,
-	"wakuV2EnableStoreConfirmationForMessagesSent": True,
-	}
-	payload = json.dumps(data).encode('utf-8')	
+	status_client.login("0x0c0bff93b4c526a4d70c72b47096734795fae99cd94ad23b4bf22ef5f67e3b40", "swigg4@1234")
+	time.sleep(0.2)
 
-	response = status_go.InitializeApplication(payload)
-	response = json.loads(response)
-	print(f"InitializeApplication:{response}")
-	time.sleep(5)
+	# status_client.sendContactRequest("0x04c13e582c51cfd8185079b3136f7ce007683a3068788e09234069dda6e0dfc1040ca0308aa8948475f2f73ff1900ca4d2f36d46a484239731413d89dda84b2f6b", "Hello there")
+	# time.sleep(0.2)
 
-	data = {'rootDataDir': './', 'kdfIterations': 256,
-			'deviceName': 'restaurant-pc-7', 'displayName': 'Restaurant7',
-			'password': 'swigg4@1234', "customizationColor":"blue",
-			'wakuV2Nameserver':'8.8.8.8', 'wakuV2Fleet':'status.prod'}
+	status_client.createOneToOneChat("0x04c13e582c51cfd8185079b3136f7ce007683a3068788e09234069dda6e0dfc1040ca0308aa8948475f2f73ff1900ca4d2f36d46a484239731413d89dda84b2f6b")
+	time.sleep(0.2)
 
-	payload = json.dumps(data).encode('utf-8')	
+	status_client.sendChatMessage("0x04c13e582c51cfd8185079b3136f7ce007683a3068788e09234069dda6e0dfc1040ca0308aa8948475f2f73ff1900ca4d2f36d46a484239731413d89dda84b2f6b", "Good morning my friend!")
+	time.sleep(0.2)
 
-	response = status_go.CreateAccountAndLogin(payload)
-	response = json.loads(response)
-	print(f"CreateAccountAndLogin:{response}")
-	time.sleep(5)
+	# msg = launchModel()
+	# print(f"MSG:{msg}")
 
-	data = {
-	"password": "swigg4@1234",
-	"keyUid": "0x0c0bff93b4c526a4d70c72b47096734795fae99cd94ad23b4bf22ef5f67e3b40",
-	"wakuV2Nameserver": "8.8.8.8",
-	"wakuV2Fleet": "status.prod"
-	}
-	payload = json.dumps(data).encode('utf-8')	
-
-	response = status_go.LoginAccount(payload)
-	response = json.loads(response)
-	print(f"LoginAccount:{response}")
-	time.sleep(5)
-
-	data = {
-	"method": "wakuext_startMessenger",
-	"params": [],
-	}
-	payload = json.dumps(data).encode('utf-8')	
-
-	response = status_go.CallRPC(payload)
-	response = json.loads(response)
-	print(f"wakuext_startMessenge:{response}")
-	time.sleep(5)
-
-	data = {
-	"method": "wakuext_sendContactRequest",
-	"params": [
-		{
-			"id": "0x04c13e582c51cfd8185079b3136f7ce007683a3068788e09234069dda6e0dfc1040ca0308aa8948475f2f73ff1900ca4d2f36d46a484239731413d89dda84b2f6b",
-			"message": "Hi, Pop",
-		}
-	]	
-	}
-	payload = json.dumps(data).encode('utf-8')	
-	response = status_go.CallPrivateRPC(payload)
-	response = json.loads(response)
-	print(f"wakuext_sendContactRequest:{response}")
-	time.sleep(5)
-
-	data = {
-	"method": "chat_createOneToOneChat",
-	"params": [
-		"",
-		"0x04c13e582c51cfd8185079b3136f7ce007683a3068788e09234069dda6e0dfc1040ca0308aa8948475f2f73ff1900ca4d2f36d46a484239731413d89dda84b2f6b",
-		""
-	]	
-	}
-	payload = json.dumps(data).encode('utf-8')	
-	response = status_go.CallPrivateRPC(payload)
-	response = json.loads(response)
-	print(f"chat_createOneToOneChat:{response}")
-	time.sleep(5)
-
-	data = {
-	"method": "wakuext_sendChatMessage",
-	"params": [
-		{
-			"chatId": "0x04c13e582c51cfd8185079b3136f7ce007683a3068788e09234069dda6e0dfc1040ca0308aa8948475f2f73ff1900ca4d2f36d46a484239731413d89dda84b2f6b",
-			"text": "Hope it was not too smelly!",
-			"contentType": 1
-		}
-	]	
-	}
-	payload = json.dumps(data).encode('utf-8')	
-
-	response = status_go.CallPrivateRPC(payload)
-	response = json.loads(response)
-	print(f"wakuext_sendChatMessage:{response}")
-
-	time.sleep(100000000)
-
+	try:
+		while True:
+			time.sleep(0.2)
+	except KeyboardInterrupt:
+		status_client.stop()
 
 if __name__ == '__main__':
 	main()
