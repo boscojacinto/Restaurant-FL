fea6a67c664000ae17db1674c250325a3a04df14
BoscoJacinto <boscojac@gmail.com>
2025-05-22 17:47:06 +0530

    Created psi class
    Signed-off-by: BoscoJacinto <boscojac@gmail.com>

--- restaurant_waku/bootstrap/client_psi.py
+++ restaurant_waku/bootstrap/client_psi.py
@@ -5,33 +5,25 @@ import json
 import ctypes
 import base64
 from ctypes import CDLL
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
-import restaurant_pb2
 
-WAKU_GO_LIB = "./libgowaku.so.0"
-DISC_URL = "enrtree://AKP74RJLRUIRLPUD3KHFKX23B5LKQYSTWE4KPXZUMJQZSLG4LYMY2@nodes.restaurants.com"
-DISC_NAMESERVER = "nodes.restaurants.com"
-DISC_ENABLE = True
+WAKU_GO_LIB = "../libgowaku.so.0"
 
 HOST = "192.168.1.26"
-SIGNING_KEY = '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef' #TODO: change later
 NODE_KEY = '0cc3ac3071d6da231a1e43849afed349ed00c3b9e289147598b653eb7092c52c'
 
-SETUP_PORT = 60000
+SETUP_PORT = 60010
 SETUP_STORE = True
 SETUP_STORE_TIME = (30*24*60*60) # 30 days
-SETUP_CLUSTER_ID = 89
+SETUP_CLUSTER_ID = 88
 SETUP_SHARD_ID = 1
 
-PSI_PORT = 70000
+PSI_PORT = 60020
 PSI_STORE = True
 PSI_STORE_TIME = (5*60) # 5 minutes
 PSI_CLUSTER_ID = 89
 PSI_SHARD_ID = 1
 
 TASTEBOT_PUBSUB_TOPIC = '/tastbot/1/customer-intersect/proto'
-PEER_ID = "16Uiu2HAkxY2C8d1ycecQnBZqXcuSo2V8rUBsioEwHvA6cZ8JFXXJ"
-PEER_PORT = "45365"
 
 WakuCallBack = ctypes.CFUNCTYPE(
 	None,
@@ -113,21 +105,20 @@ def main():
 
 	waku_lib_init()
 
-	(setup_ctx, setup_connected, setup_peer_id,
-	 setup_address, setup_content_topic) = init_setup_node()
-	print(f"Started PSI node: {setup_peer_id}, {setup_address}, {setup_content_topic}")
+	(setup_ctx, setup_peer_id, setup_address) = init_setup_node()
+	print(f"Started PSI node: {setup_peer_id}, {setup_address}, {TASTEBOT_PUBSUB_TOPIC}")
 
-	# time.sleep(4)
+	time.sleep(2)
 
-	# (psi_ctx, psi_connected, psi_address) = init_psi_node()
-	# print(f"Started PSI node: {psi_address}")
+	(psi_ctx, psi_peer_id, psi_address) = init_psi_node()
+	print(f"Started PSI node: {psi_peer_id}, {psi_address}, {TASTEBOT_PUBSUB_TOPIC}")
 
 	while True:
 		time.sleep(1)
 
 def init_setup_node():
-	node_config = "{ \"host\": \"%s\", \"port\": %d, \"nodeKey\": \"%s\", \"store\": %s, \"clusterID\": %d, \"shards\": [%d], \"dnsDiscoveryURLs\": [\"%s\"], \"dnsDiscoveryNameServer\": \"%s\", \"discV5\": %s}" \
-				   % (HOST, int(SETUP_PORT), NODE_KEY, "true" if SETUP_STORE else "false", int(SETUP_CLUSTER_ID), int(SETUP_SHARD_ID), DISC_URL, DISC_NAMESERVER, "true" if DISC_ENABLE else "false")
+	node_config = "{ \"host\": \"%s\", \"port\": %d, \"nodeKey\": \"%s\", \"store\": %s, \"clusterID\": %d, \"shards\": [%d]}" \
+				   % (HOST, int(SETUP_PORT), NODE_KEY, "true" if SETUP_STORE else "false", int(SETUP_CLUSTER_ID), int(SETUP_SHARD_ID))
 	node_config = node_config.encode('ascii')
 
 	ctx = waku_go.waku_new(node_config, wakuCallBack, None)
@@ -138,38 +129,17 @@ def init_setup_node():
 
 	peer_id = ctypes.c_char_p(None)
 	ret = waku_go.waku_peerid(ctx, wakuCallBack, ctypes.byref(peer_id))
+	peer_id = peer_id.value.decode('utf-8')
 
-	# address = ctypes.c_char_p(None)
-	# ret = waku_go.waku_listen_addresses(ctx, wakuCallBack, ctypes.byref(address))
+	address = ctypes.c_char_p(None)
+	ret = waku_go.waku_listen_addresses(ctx, wakuCallBack, ctypes.byref(address))
+	address = address.value.decode('utf-8')
 
-	discovered_nodes = ctypes.c_char_p(None)
-	waku_go.waku_dns_discovery(ctx, url, nameserver, timeout, wakuCallBack, ctypes.byref(discovered_nodes))
-	print(f"discovered_nodes:{discovered_nodes.value}")
-	time.sleep(2)
+	subscription = "{ \"pubsubTopic\": \"%s\"}" % TASTEBOT_PUBSUB_TOPIC
+	subscription = subscription.encode('ascii')
+	ret = waku_go.waku_relay_subscribe(ctx, subscription, wakuCallBack, None)
 
-	peers_list = ctypes.c_char_p(None)
-	waku_go.waku_peers(ctx, wakuCallBack, ctypes.byref(peers_list))
-	print(f"peers_list:{peers_list.value}")
-
-	# peer_list = json.loads(discovered_nodes.value.decode('utf-8'))
-	
-	# peer_str = f"{peer_list[1]['multiaddrs'][0]}/p2p/{peer_list[1]['peerID']}"
-	# print(f"\n\npeer_str:{peer_str}")
-	# peer = ctypes.c_char_p(peer_str.encode('utf-8'))
-	# connected = waku_go.waku_connect(ctx, peer, 20000, wakuCallBack, None)
-
-	# topic = get_setup_content_topic()
-
-	# subscription = "{ \"pubsubTopic\": \"%s\", \"contentTopics\":[\"%s\"]}" % (TASTEBOT_PUBSUB_TOPIC, topic.decode('utf-8'))
-	# subscription = subscription.encode('ascii')
-
-	# ret = waku_go.waku_relay_subscribe(ctx, subscription, wakuCallBack, None)
-
-	connected = True
-	peer_id = 1
-	address = 1 
-	topic = 1
-	return ctx, connected, peer_id, address, topic
+	return ctx, peer_id, address
 
 def init_psi_node():
 	node_config = "{ \"host\": \"%s\", \"port\": %d, \"store\": %s, \"clusterID\": %d, \"shards\": [%d]}" \
@@ -184,33 +154,17 @@ def init_psi_node():
 
 	peer_id = ctypes.c_char_p(None)
 	ret = waku_go.waku_peerid(ctx, wakuCallBack, ctypes.byref(peer_id))
+	peer_id = peer_id.value.decode('utf-8')
 
 	address = ctypes.c_char_p(None)
 	ret = waku_go.waku_listen_addresses(ctx, wakuCallBack, ctypes.byref(address))
+	address = address.value.decode('utf-8')
 
-	return ctx, peer_id, address.value 
+	subscription = "{ \"pubsubTopic\": \"%s\"}" % TASTEBOT_PUBSUB_TOPIC
+	subscription = subscription.encode('ascii')
+	ret = waku_go.waku_relay_subscribe(ctx, subscription, wakuCallBack, None)
 
-def get_setup_content_topic():
-	app_name = ctypes.c_char_p("tastebot".encode('utf-8'))
-	app_version = ctypes.c_char_p("1".encode('utf-8'))
-	topic_name = ctypes.c_char_p("setup".encode('utf-8'))
-	encoding = ctypes.c_char_p('proto'.encode('utf-8'))
-
-	content_topic = ctypes.c_char_p(None)
-	ret = waku_go.waku_content_topic(app_name, app_version, topic_name,
-									 encoding, wakuCallBack, ctypes.byref(content_topic))
-	return content_topic.value
-
-def get_request_content_topic(timestamp):
-	app_name = ctypes.c_char_p("tastebot".encode('utf-8'))
-	app_version = ctypes.c_char_p("1".encode('utf-8'))
-	topic_name = ctypes.c_char_p(f"request-{timestamp}".encode('utf-8'))
-	encoding = ctypes.c_char_p('proto'.encode('utf-8'))
-
-	content_topic = ctypes.c_char_p(None)
-	ret = waku_go.waku_content_topic(app_name, app_version, topic_name,
-									 encoding, wakuCallBack, ctypes.byref(content_topic))
-	return content_topic.value
+	return ctx, peer_id, address 
 
 def waku_lib_init():
 	global waku_go
@@ -220,31 +174,17 @@ def waku_lib_init():
 	waku_go.waku_new.restype = ctypes.c_void_p
 	waku_go.waku_start.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_start.restype = ctypes.c_int
+	waku_go.waku_set_event_callback.argtypes = [ctypes.c_void_p, WakuCallBack]
 	waku_go.waku_peerid.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_peerid.restype = ctypes.c_int
 	waku_go.waku_listen_addresses.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_listen_addresses.restype = ctypes.c_int
-	waku_go.waku_content_topic.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p,
-										   ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_content_topic.restype = ctypes.c_int
-	waku_go.waku_default_pubsub_topic.argtypes = [WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_default_pubsub_topic.restype = ctypes.c_int
-	waku_go.waku_dns_discovery.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_char_p,
-										   ctypes.c_int, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_dns_discovery.restype = ctypes.c_int
-	waku_go.waku_connect.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_int, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_connect.restype = ctypes.c_int
 	waku_go.waku_peers.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_peers.restype = ctypes.c_int
 	waku_go.waku_peer_cnt.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_peer_cnt.restype = ctypes.c_int
 	waku_go.waku_relay_subscribe.argtypes = [ctypes.c_void_p, ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_relay_subscribe.restype = ctypes.c_int
-	waku_go.waku_set_event_callback.argtypes = [ctypes.c_void_p, WakuCallBack]
-	waku_go.waku_relay_topics.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_relay_topics.restype = ctypes.c_int
-	waku_go.waku_store_local_query.argtypes = [ctypes.c_void_p, ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_store_local_query.restype = ctypes.c_int
 
 if __name__ == "__main__":
 	main()
--- restaurant_waku/neighbor/client_psi.py
+++ restaurant_waku/neighbor/client_psi.py
@@ -1,32 +1,32 @@
 import os
 import sys
+import time
 import json
 import ctypes
-import time
-import asyncio
 import base64
-import ecdsa
 from ctypes import CDLL
-import pytz
 from datetime import datetime
-import secrets
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../'))
-from neighbor_restauarnt import RestaurantNeighbor, restaurant_setup, restaurant_key, signing_key 
 import restaurant_pb2
-import message_pb2
-
-# Use reference from go-waku https://github.com/waku-org/go-waku/blob/master/examples/c-bindings/main.c 
-# Use PSI repo for reference for messages https://github.com/OpenMined/PSI
-# Use neighbor_restaurant.py for PSI package api references and usuage with customer list
 
 WAKU_GO_LIB = "../libgowaku.so.0"
-HOST = "192.168.1.26"
-PORT = 0
-IS_STORE = True
-PEER_ID = "16Uiu2HAm85Z14GrGtobwAD48wcazYXiuRMCjmjMHLFU7W6UCnfsr"
-PEER_PORT = "42267"
-CLUSTER_ID = 89
-SHARD_ID = 1
+DISC_URL = "enrtree://AKP74RJLRUIRLPUD3KHFKX23B5LKQYSTWE4KPXZUMJQZSLG4LYMY2@nodes.restaurants.com"
+DISC_NAMESERVER = "nodes.restaurants.com"
+DISC_ENABLE = True
+
+SETUP_PORT = 60011
+SETUP_STORE = True
+SETUP_STORE_TIME = (30*24*60*60) # 30 days
+SETUP_CLUSTER_ID = 88
+SETUP_SHARD_ID = 1
+
+PSI_PORT = 60021
+PSI_STORE = True
+PSI_STORE_TIME = (5*60) # 5 minutes
+PSI_CLUSTER_ID = 89
+PSI_SHARD_ID = 1
+
+TASTEBOT_PUBSUB_TOPIC = '/tastbot/1/customer-intersect/proto'
 
 WakuCallBack = ctypes.CFUNCTYPE(
 	None,
@@ -35,47 +35,185 @@ WakuCallBack = ctypes.CFUNCTYPE(
 	ctypes.c_void_p
 )
 
-def main():	
-	@WakuCallBack
-	def wakuCallBack(ret_code, msg: str, user_data):
-		if ret_code != 0:
-			print(f"Error: {ret_code}, msg:{msg}")
+waku_go = None
+setup_content_topic = None
+psi_request_topic = None 
 
-		if not user_data:
-			print("user data is null")
+class PSIClient:
+    def __init__(self, setup_bs_enr, psi_bs_enr, node_key, host):
+    	self.setup_bs_enr = setup_bs_enr
+    	self.psi_bs_enr = psi_bs_enr
+    	self.node_key = node_key
+    	self.host = host
+    	waku_lib_init()
+
+    def start(self):
+    	(setup_ctx, setup_connected, setup_peer_id,
+    		setup_address, setup_content_topic) = self.init_setup_node()
+    	print(f"Started PSI node: {setup_peer_id}, {setup_address}, {setup_content_topic}")
+
+    	(psi_ctx, psi_connected, psi_peer_id,
+    		psi_address, psi_content_topic) = self.init_psi_node()
+    	print(f"Started PSI node: {psi_peer_id}, {psi_address}, {psi_content_topic}")
+
+    def init_setup_node(self):
+    	node_config = "{ \"host\": \"%s\", \"port\": %d, \"nodeKey\": \"%s\", \"store\": %s, \"clusterID\": %d, \"shards\": [%d], \"discV5BootstrapNodes\": [\"%s\"]}" \
+    					% (self.host, int(SETUP_PORT), self.node_key, "true" if SETUP_STORE else "false", int(SETUP_CLUSTER_ID), int(SETUP_SHARD_ID), self.setup_bs_enr)
+    	node_config = node_config.encode('ascii')
+
+    	ctx = waku_go.waku_new(node_config, wakuCallBack, None)
+
+    	ret = waku_go.waku_set_event_callback(ctx, setupEventCallBack)
+
+    	ret = waku_go.waku_start(ctx, wakuCallBack, None)
+
+    	peer_id = ctypes.c_char_p(None)
+    	ret = waku_go.waku_peerid(ctx, wakuCallBack, ctypes.byref(peer_id))
+    	peer_id = peer_id.value.decode('utf-8')
+
+    	address = ctypes.c_char_p(None)
+    	ret = waku_go.waku_listen_addresses(ctx, wakuCallBack, ctypes.byref(address))
+    	address = address.value.decode('utf-8')
+
+    	peers_list = ctypes.c_char_p(None)
+    	waku_go.waku_peers(ctx, wakuCallBack, ctypes.byref(peers_list))
+    	print(f"peers_list:{peers_list.value}")
+
+    	peer_str = "/ip4/192.168.1.26/tcp/60010/p2p/16Uiu2HAmHk5rdpnfYGDh2XchPsQxvqB3j4zb9owzfFjV7fMWbQNs"
+    	peer = ctypes.c_char_p(peer_str.encode('utf-8'))
+    	connected = waku_go.waku_connect(ctx, peer, 20000, wakuCallBack, None)
+
+    	topic = self.get_setup_content_topic()
+
+    	subscription = "{ \"pubsubTopic\": \"%s\", \"contentTopics\":[\"%s\"]}" % (TASTEBOT_PUBSUB_TOPIC, topic.decode('utf-8'))
+    	subscription = subscription.encode('ascii')
+    	ret = waku_go.waku_relay_subscribe(ctx, subscription, wakuCallBack, None)
+
+    	return ctx, connected, peer_id, address, topic
+
+    def init_psi_node(self):
+    	node_config = "{ \"host\": \"%s\", \"port\": %d, \"nodeKey\": \"%s\", \"store\": %s, \"clusterID\": %d, \"shards\": [%d], \"discV5BootstrapNodes\": [\"%s\"]}" \
+    					% (self.host, int(PSI_PORT), self.node_key, "true" if PSI_STORE else "false", int(PSI_CLUSTER_ID), int(PSI_SHARD_ID), self.psi_bs_enr)
+
+    	node_config = node_config.encode('ascii')
+
+    	ctx = waku_go.waku_new(node_config, wakuCallBack, None)
+
+    	ret = waku_go.waku_set_event_callback(ctx, psiEventCallBack)
+
+    	ret = waku_go.waku_start(ctx, wakuCallBack, None)
+
+    	peer_id = ctypes.c_char_p(None)
+    	ret = waku_go.waku_peerid(ctx, wakuCallBack, ctypes.byref(peer_id))
+    	peer_id = peer_id.value.decode('utf-8')
+
+    	address = ctypes.c_char_p(None)
+    	ret = waku_go.waku_listen_addresses(ctx, wakuCallBack, ctypes.byref(address))
+    	address = address.value.decode('utf-8')
+
+    	peer_str = "/ip4/192.168.1.26/tcp/60020/p2p/16Uiu2HAm4vuRBjEzZ82WP44gbY6B1qBvPPun5r2eWBfe4EhmYTFQ"
+    	peer = ctypes.c_char_p(peer_str.encode('utf-8'))
+    	connected = waku_go.waku_connect(ctx, peer, 20000, wakuCallBack, None)
+
+    	current_time = datetime.now().strftime("%H:%M:%S")	
+    	topic = self.get_psi_content_topic(current_time)
+
+    	subscription = "{ \"pubsubTopic\": \"%s\", \"contentTopics\":[\"%s\"]}" % (TASTEBOT_PUBSUB_TOPIC, topic.decode('utf-8'))
+    	subscription = subscription.encode('ascii')
+    	ret = waku_go.waku_relay_subscribe(ctx, subscription, wakuCallBack, None)
+
+    	return ctx, connected, peer_id, address, topic 
+
+    def get_setup_content_topic(self):
+    	app_name = ctypes.c_char_p("tastebot".encode('utf-8'))
+    	app_version = ctypes.c_char_p("1".encode('utf-8'))
+    	topic_name = ctypes.c_char_p("setup".encode('utf-8'))
+    	encoding = ctypes.c_char_p('proto'.encode('utf-8'))
+
+    	content_topic = ctypes.c_char_p(None)
+    	ret = waku_go.waku_content_topic(app_name, app_version, topic_name,
+    		encoding, wakuCallBack, ctypes.byref(content_topic))
+    	return content_topic.value
+
+    def get_psi_content_topic(self, timestamp):
+    	app_name = ctypes.c_char_p("tastebot".encode('utf-8'))
+    	app_version = ctypes.c_char_p("1".encode('utf-8'))
+    	topic_name = ctypes.c_char_p(f"psi-{timestamp}".encode('utf-8'))
+    	encoding = ctypes.c_char_p('proto'.encode('utf-8'))
+
+    	content_topic = ctypes.c_char_p(None)
+    	ret = waku_go.waku_content_topic(app_name, app_version, topic_name,
+    		encoding, wakuCallBack, ctypes.byref(content_topic))
+    	return content_topic.value
+
+@WakuCallBack
+def wakuCallBack(ret_code, msg: str, user_data):
+	if ret_code != 0:
+		print(f"Error: {ret_code}, msg:{msg}")
+
+	if not user_data:
+		return
+
+	data_ref = ctypes.cast(user_data, ctypes.POINTER(ctypes.c_char_p))
+	data_ptr = data_ref[0]
+
+	if data_ref.contents:
+		data_ref.contents = None
+
+	if msg:
+		msg_ptr = ctypes.create_string_buffer(msg)
+
+		if not msg_ptr:
 			return
 
-		if not msg:
-			print(f"msg: {msg}")
-			return
-
-		data_ref = ctypes.cast(user_data, ctypes.POINTER(ctypes.c_char_p))
+		data_ref[0] = ctypes.cast(msg_ptr, ctypes.c_char_p)
 		data_ptr = data_ref[0]
 
-		# if data_ref.contents:
-		# 	libc.free(data_ref.contents)
-		# 	data_ref.contents = None
+@WakuCallBack
+def setupEventCallBack(ret_code, msg: str, user_data):
+	print(f"EVENT ret: {ret_code}, msg: {msg}, user_data:{user_data}")
+	if ret_code != 0:
+		return
 
-		if msg:
-			msg_str = msg.decode('utf-8')
-			msg_len = len(msg_str)
+	event_str = msg.decode('utf-8')
+	event = json.loads(event_str)
+	if event['type'] == "message":
+		msgId = event['event']['messageId']
+		pubsub_topic = event['event']['pubsubTopic']
+		waku_message = event['event']['wakuMessage']
+		print(f"messageId:{msgId}")
+		if pubsub_topic == TASTEBOT_PUBSUB_TOPIC:
+			content_topic = waku_message['contentTopic']
+			payload_b64 = waku_message['payload']
+			if content_topic == "/tastebot/1/customer-list/proto":
+				print(f"Setup (server) -- instore")
+			else:
+				print(f"Other")
 
-			msg_ptr = ctypes.create_string_buffer(msg_len)
+@WakuCallBack
+def psiEventCallBack(ret_code, msg: str, user_data):
+	print(f"EVENT ret: {ret_code}, msg: {msg}, user_data:{user_data}")
+	if ret_code != 0:
+		return
 
-			if not msg_ptr:
-				return
+	event_str = msg.decode('utf-8')
+	event = json.loads(event_str)
+	if event['type'] == "message":
+		msgId = event['event']['messageId']
+		pubsub_topic = event['event']['pubsubTopic']
+		waku_message = event['event']['wakuMessage']
+		print(f"messageId:{msgId}")
+		if pubsub_topic == TASTEBOT_PUBSUB_TOPIC:
+			content_topic = waku_message['contentTopic']
+			payload_b64 = waku_message['payload']
+			if content_topic == "/tastebot/1/customer-list/proto":
+				print(f"Setup (server) -- instore")
+			else:
+				print(f"Other")
 
-			ctypes.memmove(msg_ptr, msg_str.encode(), msg_len)
+def waku_lib_init():
+	global waku_go
 
-			data_ref[0] = ctypes.cast(msg_ptr, ctypes.c_char_p)
-			data_ptr = data_ref[0]
-
-	@WakuCallBack
-	def eventCallBack(ret_code, msg: str, user_data):
-		if ret_code != 0:
-			print(f"Error: {ret_code}")
-			print(f"\nEvent: {msg}")
-	
 	waku_go = CDLL(WAKU_GO_LIB)
 	waku_go.waku_new.argtypes = [ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_new.restype = ctypes.c_void_p
@@ -95,141 +233,22 @@ def main():
 	waku_go.waku_dns_discovery.restype = ctypes.c_int
 	waku_go.waku_connect.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_int, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_connect.restype = ctypes.c_int
+	waku_go.waku_peers.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
+	waku_go.waku_peers.restype = ctypes.c_int
 	waku_go.waku_peer_cnt.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_peer_cnt.restype = ctypes.c_int
 	waku_go.waku_relay_subscribe.argtypes = [ctypes.c_void_p, ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_relay_subscribe.restype = ctypes.c_int
-	waku_go.waku_encode_symmetric.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_encode_symmetric.restype = ctypes.c_int
-	waku_go.waku_relay_publish.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, WakuCallBack, ctypes.c_void_p]
-	waku_go.waku_relay_publish.restype = ctypes.c_int
+	waku_go.waku_set_event_callback.argtypes = [ctypes.c_void_p, WakuCallBack]
 	waku_go.waku_relay_topics.argtypes = [ctypes.c_void_p, WakuCallBack, ctypes.c_void_p]
 	waku_go.waku_relay_topics.restype = ctypes.c_int
-	# waku_go.waku_set_event_callback.argtypes = [ctypes.c_void_p, WakuCallBack]
-	# waku_go.waku_set_event_callback.restype = ctypes.c_void
-
-	restaurant = RestaurantNeighbor()
-	restaurant_setup()
-
-	json_config = "{ \"host\": \"%s\", \"port\": %d, \"store\": %s, \"clusterID\": %d, \"shards\": [%d]}" % (HOST, int(PORT), "true" if IS_STORE else "false", int(CLUSTER_ID), int(SHARD_ID))
-	json_config = json_config.encode('ascii')
-
-	ctx = waku_go.waku_new(json_config, wakuCallBack, None)
-	time.sleep(2)
-
-	# ret = waku_go.waku_set_event_callback(ctx, eventCallBack)
-	# print(f"setcallback:{ret}")
-
-	ret = waku_go.waku_start(ctx, wakuCallBack, None)
-	print(f"start:{ret}")
-	time.sleep(2)
-
-	peer_id = ctypes.c_char_p(None)
-	ret = waku_go.waku_peerid(ctx, wakuCallBack, ctypes.byref(peer_id))
-	print(f"peerid:{peer_id.value}")
-	time.sleep(2)
-
-	addresses = ctypes.c_char_p(None)
-	ret = waku_go.waku_listen_addresses(ctx, wakuCallBack, ctypes.byref(addresses))
-	print(f"addresses:{addresses.value}")
-	time.sleep(2)
-
-	default_pubsub_topic = ctypes.c_char_p(None)
-	waku_go.waku_default_pubsub_topic(wakuCallBack, ctypes.byref(default_pubsub_topic))
-	print(f"default_pubsub_topic:{default_pubsub_topic.value}")
-
-	# pubsub_topic = '/tastbot/1/neighbor-1/proto'
-	# app_name_str = "tastebot"
-	# app_version_str = "1"
-	# topic_name_str = "customer-list"
-	# encoding_str = "proto"
-	# app_name = ctypes.c_char_p(app_name_str.encode('utf-8'))
-	# app_version = ctypes.c_char_p(app_version_str.encode('utf-8'))
-	# topic_name = ctypes.c_char_p(topic_name_str.encode('utf-8'))
-	# encoding = ctypes.c_char_p(encoding_str.encode('utf-8'))
-
-	# content_topic = ctypes.c_char_p(None)
-	# ret = waku_go.waku_content_topic(app_name, app_version, topic_name,
-	# 								 encoding, wakuCallBack, ctypes.byref(content_topic))
-	# print(f"content topic:{content_topic.value}")
-	# time.sleep(2)
-
-	# # url_str = "enrtree://AOGYWMBYOUIMOENHXCHILPKY3ZRFEULMFI4DOM442QSZ73TT2A7VI@test.waku.nodes.status.im"
-	# url_str = "enrtree://AIRVQ5DDA4FFWLRBCHJWUWOO6X6S4ZTZ5B667LQ6AJU6PEYDLRD5O@sandbox.waku.nodes.status.im"
-	# nameserver_str = ""
-	# timeout = 20000
-	# url = ctypes.c_char_p(url_str.encode('utf-8'))
-	# nameserver = ctypes.c_char_p(nameserver_str.encode('utf-8'))
-
-	# discovered_nodes = ctypes.c_char_p(None)
-	# waku_go.waku_dns_discovery(ctx, url, nameserver, timeout, wakuCallBack, ctypes.byref(discovered_nodes))
-	# print(f"discovered_nodes:{discovered_nodes.value}")
-	# time.sleep(2)
-
-	# peer_str = f"/ip4/192.168.1.26/tcp/{PEER_PORT}/p2p/{PEER_ID}"
-	# peer = ctypes.c_char_p(peer_str.encode('utf-8'))
-	# ret = waku_go.waku_connect(ctx, peer, 20000, wakuCallBack, None)
-	# print(f"connect:{ret}")
-	# time.sleep(2)
-
-	# subscription = "{ \"pubsubTopic\": \"%s\", \"contentTopics\":[\"%s\"]}" % (pubsub_topic, content_topic.value.decode('utf-8'))
-	# subscription = subscription.encode('ascii')
-	# print(f"subscription:{subscription}")
-
-	# ret = waku_go.waku_relay_subscribe(ctx, subscription, wakuCallBack, None)
-	# print(f"ret:{ret}")
-	# time.sleep(2)
-
-	# stopics = ctypes.c_char_p(None)
-	# waku_go.waku_relay_topics(ctx, wakuCallBack, ctypes.byref(stopics))
-	# print(f"stopics:{stopics.value}")
-	# time.sleep(2)
-
-	# setup_request = restaurant_pb2.SetupRequest(num_customers=1)
-	# setup_msg = asyncio.run(restaurant.Setup(setup_request))
-	# print(f"\nsetup_msg:{setup_msg}")
-	# setup_reply = restaurant_pb2.SetupReply()
-	# setup_reply.ParseFromString(setup_msg)
-	# print(f"restaurantKey:{setup_reply.restaurantKey}")
-	# payload = base64.b64encode(setup_msg).decode()
-	# print(f"payload:{payload}")
-
-	# waku_msg_str = "{ \"payload\":\"%s\",\"contentTopic\":\"%s\",\"timestamp\":%d}" % (payload, content_topic.value.decode('utf-8'), int(0))
-	# waku_msg_ptr = waku_msg_str.encode('ascii')
-	# print(f"waku_msg_ptr:{waku_msg_ptr}")
-
-	# pubsub_topic_ptr = ctypes.c_char_p(pubsub_topic.encode('utf-8'))
-	# print(f"pubsub_topic_ptr:{pubsub_topic_ptr}")
-	
-	# key = ctypes.c_char_p(signing_key.encode('utf-8'))
-	# print(f"key:{key})")
-
-	# encoded_msg = ctypes.c_char_p(None)
-	# ret = waku_go.waku_encode_symmetric(waku_msg_ptr, key, None, wakuCallBack, ctypes.byref(encoded_msg))
-	# print(f"encoding:{ret}")
-	# print(f"encoded_msg:{encoded_msg.value}")
-	# print(f"encoded_msg1:{encoded_msg}")
-
-	# message_id = ctypes.c_char_p(None)
-	# ret = waku_go.waku_relay_publish(ctx, encoded_msg, pubsub_topic_ptr, 0, wakuCallBack, ctypes.byref(message_id))
-	# print(f"publish:{ret}")
-
-
-	# time.sleep(10)
-
-	# store_query = '{ "pubsubTopic": "%s", "pagingOptions": {"pageSize": 40, "forward":false}}' % pubsub_topic
-	# #store_query = "{ \"pubsubTopic\": \"%s\"}" % (pubsub_topic)
-	# #store_query = store_query.encode('utf-8')
-	# store_query_ptr = ctypes.c_char_p(store_query.encode('utf-8'))
-	# print(f"\nstore_query_ptr:{store_query_ptr}")
-	# local_store = ctypes.c_char_p(None)
-	# print(f"ctx:{ctx}") 
-	# ret = waku_go.waku_store_local_query(ctx, store_query_ptr, wakuCallBack, ctypes.byref(local_store))
-	# print(f"RET:{ret}")
-	# print(f"local_store:{local_store.value}")
-
-	while True:
-		time.sleep(1)
+	waku_go.waku_store_local_query.argtypes = [ctypes.c_void_p, ctypes.c_char_p, WakuCallBack, ctypes.c_void_p]
+	waku_go.waku_store_local_query.restype = ctypes.c_int
 
 if __name__ == "__main__":
-	main()
+	setup_bs_enr = "-Jq4QJfs2uKrvjdhRyuHfB8JxBv-AxzXLAKIwJOoaz9k0sb_ZWLB0sKQJaqHYyAucAoUD3P36q8BRPitOvutyZpj7u2GAZb3wmK0gmlkgnY0gmlwhMCoARqCcnOFAFgBAAGJc2VjcDI1NmsxoQNLmJB1Pj72eUSZQnMof-AJdmltBsVrqCSzGa_k_YI8UIN0Y3CC6mqFd2FrdTID"
+	psi_bs_enr = "-Jq4QCeXiHyMKcbWSA5F-NWp4PiHDMcfukz3irHoIwKMePJzJwOKSrB1IwGeHWFL7mIr5kwGuvYcqwcovcEeeqbq3VyGAZb3wmq3gmlkgnY0gmlwhMCoARqCcnOFAFkBAAGJc2VjcDI1NmsxoQKNNjhPvRAVb8pSV4ssOmKJ7xncEJn69ztPqazHXT01Q4N0Y3CC6nSFd2FrdTID"
+	node_key = '4ddecde332eff9353c8a7df4b429299af13bbfe2f5baa7f4474c93faf2fea0b5'
+	host = "192.168.1.26"
+	client = PSIClient(setup_bs_enr, psi_bs_enr, node_key, host)
+	client.start()
